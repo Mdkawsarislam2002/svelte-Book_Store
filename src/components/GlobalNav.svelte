0a063654dccb264b598758363bd4d73011d5e36a
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2023-05-15 13:35:42 +0600

    Convert html to svelte components

---
+++ src/components/GlobalNav.svelte
@@ -0,0 +1,43 @@
+<script>
+  import logo from "../assets/logo.svg";
+</script>
+
+<nav class="py-4 2xl:px-6">
+  <div class="container flex items-center justify-between">
+    <a href="/">
+      <img src={logo} alt="logo" width="150px" class="object-contain" />
+    </a>
+
+    <ul class="hidden md:flex items-center space-x-6">
+      <a class="font-semibold cursor-pointer" href="/" id="lws-bookStore">
+        <li>Book Store</li>
+      </a>
+      <a class="cursor-pointer" href="/add" id="lws-addBook">
+        <li>Add Book</li>
+      </a>
+    </ul>
+
+    <form class="flex items-center">
+      <div class="group relative rounded-md bg-white">
+        <svg
+          width="20"
+          height="20"
+          fill="currentColor"
+          class="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-primary"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+          />
+        </svg>
+        <input
+          type="text"
+          placeholder="Filter books..."
+          class="search"
+          id="lws-search"
+        />
+      </div>
+    </form>
+  </div>
+</nav>
---
+++ src/routes/add/+page.svelte
@@ -0,0 +1,111 @@
+<script>
+  let title = "";
+  let author = "";
+  let imgUrl = "";
+  let price = "";
+  let rating = "";
+  let IsFeatured = "";
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    console.table({
+      title,
+      author,
+      imgUrl,
+      price,
+      rating,
+      IsFeatured,
+    });
+  };
+</script>
+
+<main class="py-6 2xl:px-6">
+  <div class="container">
+    <div
+      class="p-8 overflow-hidden bg-white shadow-cardShadow rounded-md max-w-xl mx-auto"
+    >
+      <h4 class="mb-8 text-xl font-bold text-center">Add New Book</h4>
+      <form class="book-form" on:submit={formHandler}>
+        <div class="space-y-2">
+          <label for="lws-bookName">Book Name</label>
+          <p />
+          <input
+            required
+            class="text-input"
+            type="text"
+            id="lws-bookName"
+            name="name"
+            bind:value={title}
+          />
+        </div>
+
+        <div class="space-y-2">
+          <label for="lws-author">Author</label>
+          <input
+            required
+            class="text-input"
+            type="text"
+            id="lws-author"
+            name="author"
+            bind:value={author}
+          />
+        </div>
+
+        <div class="space-y-2">
+          <label for="lws-thumbnail">Image Url</label>
+          <input
+            required
+            class="text-input"
+            type="text"
+            id="lws-thumbnail"
+            name="thumbnail"
+            bind:value={imgUrl}
+          />
+        </div>
+
+        <div class="grid grid-cols-2 gap-8 pb-4">
+          <div class="space-y-2">
+            <label for="lws-price">Price</label>
+            <input
+              required
+              class="text-input"
+              type="number"
+              id="lws-price"
+              name="price"
+              bind:value={price}
+            />
+          </div>
+
+          <div class="space-y-2">
+            <label for="lws-rating">Rating</label>
+            <input
+              required
+              class="text-input"
+              type="number"
+              id="lws-rating"
+              name="rating"
+              min="1"
+              max="5"
+              bind:value={rating}
+            />
+          </div>
+        </div>
+
+        <div class="flex items-center">
+          <input
+            id="lws-featured"
+            type="checkbox"
+            name="featured"
+            class="w-4 h-4"
+            bind:checked={IsFeatured}
+          />
+          <label for="lws-featured" class="ml-2 text-sm">
+            This is a featured book
+          </label>
+        </div>
+
+        <button type="submit" class="submit" id="lws-submit">Add Book</button>
+      </form>
+    </div>
+  </div>
+</main>
